a19827fe88c34e5469299560340630c90ab0c2bb
Mahmut Taşkıran <taskiranmahmutt@gmail.com>
2022-08-18 21:09:57 +0300

    unit tests for RemoteMoviceService

---
+++ test/services/remote_movie_service_test.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'fake_responses.dart';
+import 'test_utils.dart';
+
+void main() {
+  test("RemoteMovieService should return MovieListResponse", () async {
+    final movieService = provideMovieService(
+      provideMovieDbApi(
+        provideMockClientWithResult(
+          movieListResponse,
+        ),
+      ),
+    );
+
+    final response = await movieService.getMovies('', 1);
+
+    assert(response.status == HttpStatus.ok);
+    assert(response.results.length == 1);
+  });
+
+  test("RemoteMovieService should return MovieDetails", () async {
+    final movieService = provideMovieService(
+      provideMovieDbApi(
+        provideMockClientWithResult(
+          movieDetailsResponse,
+        ),
+      ),
+    );
+
+    final response = await movieService.getMovieDetails('1');
+
+    assert(response.status == HttpStatus.ok);
+    assert(response.homepage.isNotEmpty);
+  });
+}
