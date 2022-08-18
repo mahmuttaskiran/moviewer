af029ca8a712dc46414a4de83a4349c07a4da2c2
Mahmut Taşkıran <taskiranmahmutt@gmail.com>
2022-08-18 20:08:19 +0300

    add utility getters for BaseResponse

--- lib/models/responses/base.dart
+++ lib/models/responses/base.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
 class BaseResponse {
+  static const _unknownError = "Unknown error";
+
   BaseResponse({required this.status, this.error});
 
   String? error;
   int status;
+
+  bool get isSuccess => status == HttpStatus.ok;
+
+  bool get hasError => status != HttpStatus.ok;
+
+  String get errorMessage => error ?? _unknownError;
 }
